4a451fd4bb25ba503e44226c969bb5d44786b7cb
Ali Heidari <ali.heidari.6572@gmail.com>
2024-10-22 00:59:16 +0330

    HashRouter in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './custom.css';
 import './input.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
